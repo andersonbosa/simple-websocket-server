7c96d4cde62eb813954238367fe17347a9987ce0
andersonbosa <andersonbosa0@gmail.com>
2025-03-30 20:06:00 -0300

    format code

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,19 +1,18 @@
-import readline from "readline";
-import { sendMessage } from "./client.mjs";
+import readline from "readline"
+import { sendMessage } from "./client.mjs"
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-});
+})
 
-console.log("Digite uma mensagem para enviar pelo WebSocket. Digite 'exit' para sair.");
-
+console.log("Digite uma mensagem para enviar pelo WebSocket. Digite 'exit' para sair.")
 rl.on("line", (input) => {
-    if (input.toLowerCase() === "exit") {
-        console.log("Encerrando...");
-        rl.close();
-        process.exit(0);
+    if (["exit", "q"].includes(input.toLowerCase())) {
+        console.log("Encerrando...")
+        rl.close()
+        process.exit(0)
     } else {
-        sendMessage(input);
+        sendMessage(input)
     }
-});
+})
